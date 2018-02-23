5df10e7e5abc177a384ee72c5b14ec5ff2505176
Carlos Alvarez Gonzalez <LitosLpr@gmail.com>
2018-02-23 09:53:13 +0100

    Iniciando clase persona

---
+++ personas.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class personas here.
+ *
+ * @author (Carlos Alvarez)
+ * @version (23/02/2018)
+ */
+public class personas
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class personas
+     */
+    public personas()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
